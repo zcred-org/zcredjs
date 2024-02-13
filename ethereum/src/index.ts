1408a3bac07b499eda944b5aa268fd61847ca4fb
Pave1Pal <pd271828@gmail.com>
2024-02-13 16:53:09 +0300

    chore(ethereum): unique signature format

--- ethereum/src/index.ts
+++ ethereum/src/index.ts
@@ -1,1 +1,14 @@
 export * from "./wallet-adapter/index.js";
+import * as u8a from "uint8arrays";
+
+export const EthSignature = {
+  toBase58(hexSignature: string): string {
+    const signature = hexSignature.startsWith("0x")
+      ? hexSignature.slice(2).toLowerCase()
+      : hexSignature.toLowerCase();
+    return u8a.toString(u8a.fromString(signature, "hex"), "base58btc");
+  },
+  fromBase58(signature: string): string {
+    return "0x" + u8a.toString(u8a.fromString(signature, "base58btc"), "hex");
+  }
+};
