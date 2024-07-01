87b0ec1cdb9aad4966fdf9028955a1d0d3e69e3a
Pave1Pal <pd271828@gmail.com>
2024-07-01 21:57:34 +0300

    fix(core): save user data headre content type

--- core/src/cred-holder.ts
+++ core/src/cred-holder.ts
@@ -44,6 +44,9 @@ export class CredHolder<T extends UserData = UserData> {
   private async saveData(data: Json): Promise<{ secretId: string }> {
     const response = await fetch(this.context.userDataHolderURL, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(data)
     });
     if (!response.ok) throw new Error(
